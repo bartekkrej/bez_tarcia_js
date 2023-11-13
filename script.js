312625bb867ac00d835ab34cfca99a9d74b2d78e
Bartosz Krej <krejbartek@gmail.com>
2023-11-13 09:56:15 +0100

    klasa Vec - wektor

--- script.js
+++ script.js
@@ -24,6 +24,54 @@ function y2ekr(l) {
     return Math.round(l * GEO_D + GEO_DY)
 }
 
+class Vec {
+    // constructor(x, y){
+    //     this.x = x
+    //     this.y = y
+    //     this.val = this.getValue()
+    //     this.arg = this.getArg()
+    // }
+    // constructor(x, y, arg, val){
+    //     this.x = x
+    //     this.y = y
+    //     this.val = val
+    //     this.arg = arg
+    // }
+    constructor() {
+        this.x = 0
+        this.y = 0
+        this.val = 0
+        this.arg = 0
+    }
+
+    static fromArgValue(arg, val) {
+        let res = new Vec()
+        res.arg = arg
+        res.val = val
+        res.x = res.val * Math.cos(arg)
+        res.y = val * Math.sin(arg)
+        return res;
+    }
+
+    add(other) {
+        this.x += other.x
+        this.y += other.y
+        updateArgVal()
+    }
+
+    getValue() {
+        return Math.sqrt(this.x * this.x + this.y + this.y)
+    }
+
+    getArg() {
+        return Math.atan2(this.y, this.x)
+    }
+
+    updateArgVal() {
+        this.arg = this.getArg()
+        this.val = this.getValue()
+    }
+}
 class Form {
 
     constructor() {
